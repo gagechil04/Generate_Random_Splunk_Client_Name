cb190765d3e6a3b3200ee7ade7e409499e21dfd8
gagechil04 <115762213+gagechil04@users.noreply.github.com>
2024-04-06 23:06:24 -0400

    Update generate_client_name.sh

--- generate_client_name.sh
+++ generate_client_name.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#Echo a blank new line and then create deployment-client header
 echo >> /opt/splunk/etc/system/local/deploymentclient.conf &&
 echo "[deployment-client]" >> /opt/splunk/etc/system/local/deploymentclient.conf &&
 
@@ -14,7 +15,7 @@ random_string=$(generate_random_string)
 # Specify the path to the existing file
 file_path="/opt/splunk/etc/system/local/deploymentclient.conf"
 
-# Check if the file exists
+# Check if the file exists and append the random string to the prefix "AWS-Server" You can change the prefix to anything you would like if you need to create multiple filtering groups
 if [ -f "$file_path" ]; then
     # Append the random string to the existing file
     echo "clientName = AWS-Server$random_string" >> "$file_path"
