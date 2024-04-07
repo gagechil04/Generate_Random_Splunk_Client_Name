382581cf428772b221503be3f45d17cf14d2d97c
gagechil04 <115762213+gagechil04@users.noreply.github.com>
2024-04-06 23:00:11 -0400

    Create generate_client_name.sh

---
+++ generate_client_name.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo >> /opt/splunk/etc/system/local/deploymentclient.conf &&
+echo "[deployment-client]" >> /opt/splunk/etc/system/local/deploymentclient.conf &&
+
+# Function to generate a random alphanumerical string
+generate_random_string() {
+    cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1
+}
+
+# Generate a random string
+random_string=$(generate_random_string)
+
+# Specify the path to the existing file
+file_path="/opt/splunk/etc/system/local/deploymentclient.conf"
+
+# Check if the file exists
+if [ -f "$file_path" ]; then
+    # Append the random string to the existing file
+    echo "clientName = AWS-Server$random_string" >> "$file_path"
+    # Print a message indicating the completion of the task
+    echo "Random string has been appended to $file_path"
+else
+    # Print an error message if the file does not exist
+    echo "Error: File $file_path does not exist."
+fi
